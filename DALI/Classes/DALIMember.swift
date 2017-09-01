79a51e6398bdb421dcc27a2623e5d7b8fea7b647
John Kotz <john.lyme@mac.com>
2017-09-01 03:31:36 -0400

    Adding player id update

--- DALI/Classes/DALIMember.swift
+++ DALI/Classes/DALIMember.swift
@@ -156,4 +156,18 @@ public struct DALIMember {
 		
 		return user
 	}
+	
+	public func setNotificationID(id: String) {
+		let dict = [
+			"playerID": id
+		]
+		
+		do {
+			try ServerCommunicator.post(url: "\(DALIapi.config.serverURL)/api/users/\(self.id)/notifID", json: JSON(dict), callback: { (success, data, error) in
+				
+			})
+		}catch{
+			
+		}
+	}
 }
